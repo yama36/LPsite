4dd1a06476d0135a99c83b6d47f9543981f61be6
yama36 <inishal3654@hotmail.co.jp>
2022-05-21 10:22:59 +0900

    HBmenu complete

--- js/hbMenu.js
+++ js/hbMenu.js
@@ -1,26 +1,30 @@
-const btn = document.querySelector('.nav--top .HB__btn');
-const menu = document.querySelector('.nav--top .HB__list');
+const btn = [document.querySelector('.nav--top .HB__btn'), document.querySelector('.nav--fixd .HB__btn')];
+const menu = [document.querySelector('.nav--top .HB__list'), document.querySelector('.nav--fixd .HB__list')];
+const item = [document.querySelector('.nav--top .HB__item'), document.querySelector('.nav--fixd .HB__item')];
 
 
-// スクロール禁止
-function disableScroll(event) {
-  event.preventDefault();
-}
+btn.forEach((e, index) => {
+  e.addEventListener('click', () => {
+    if (e.className !== "HB__btn inview") {
+      e.classList.add('inview');
+      menu[index].classList.add('inview');
+      bodyScrollLock.disableBodyScroll(menu[index]);
+    } else {
+      e.classList.remove('inview');
+      menu[index].classList.remove('inview');
+      bodyScrollLock.clearAllBodyScrollLocks();
+    }
+  })
+});
 
-function hbMenu() {
-  if (btn.className !== "HB__btn inview") {
-    btn.classList.add('inview');
-    menu.classList.add('inview');
-    document.addEventListener('touchmove', disableScroll, { passive: false });
-    document.body.classList.add('hidden');
-  } else {
-    btn.classList.remove('inview');
-    menu.classList.remove('inview');
-    document.removeEventListener('touchmove', disableScroll, { passive: false });
-    document.body.classList.remove('hidden');
-  }
-}
+menu.forEach((e, index) => {
+  const child = e.children;
 
-btn.addEventListener('click', () => {
-  hbMenu();
+  for (let i = 0; i < child.length; i++) {
+    child[i].addEventListener('click', () => {
+      menu[index].classList.remove('inview');
+      btn[index].classList.remove('inview');
+      bodyScrollLock.clearAllBodyScrollLocks();
+    });
+  };
 });
