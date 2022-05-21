13fb6aa4294706c96f0e3a12dba2848310156e09
yama36 <inishal3654@hotmail.co.jp>
2022-05-22 07:34:46 +0900

    細かいところ修正

--- js/scroll.js
+++ js/scroll.js
@@ -1,7 +1,7 @@
 var ScrollrevealFade = {
 	origin: 'bottom',
 	distance: '50px',//移動範囲
-	scale: .8,//拡大表示（１で横スクロールが始まる）
+	scale: 1,//拡大表示（１で横スクロールが始まる）
 	duration: 1200, //アニメーションの速度
 	delay :200 //要素が表示するまでの待機時間（ディレイ）
 };
